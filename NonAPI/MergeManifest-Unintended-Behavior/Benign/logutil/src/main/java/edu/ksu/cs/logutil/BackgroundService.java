d2be969bcb4b4e1a14e6035180e7a981eb2165a1
Joy Mitra <joymitro.gmail.com>
2018-06-14 13:24:41 -0500

    addressed PR comments

--- NonAPI/MergeManifest-Unintended-Behavior/Benign/logutil/src/main/java/edu/ksu/cs/logutil/BackgroundService.java
+++ NonAPI/MergeManifest-Unintended-Behavior/Benign/logutil/src/main/java/edu/ksu/cs/logutil/BackgroundService.java
@@ -5,13 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.PowerManager;
 
-/**
- * An {@link IntentService} subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * <p>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
- */
 public class BackgroundService extends IntentService {
 
     public BackgroundService() {
